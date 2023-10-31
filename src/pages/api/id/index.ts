071b23ce01abfe1c65bd1909a1363efb816724fe
Juan Ignacio Fogolin <83310328+juanifogo@users.noreply.github.com>
2023-10-30 23:27:46 -0300

    test5

--- src/pages/api/id/index.ts
+++ src/pages/api/id/index.ts
@@ -2,7 +2,7 @@ import { getServerSession } from "next-auth"
 import { NextApiRequest, NextApiResponse } from "next"
 import { authOptions } from "../auth/[...nextauth]"
 
-export default async (req: NextApiRequest, res: NextApiResponse) => {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const session = await getServerSession(authOptions)
-  res.json(session)
+  return res.json(session)
 }
