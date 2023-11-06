eefed5501ff63c8ea2ea939bfbb8af5ce3596329
FOGOLIN LAGARES JUAN IGNACIO <46756494@ort>
2023-11-06 13:44:30 -0300

    try migration tp app

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,29 @@
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import EmailProvider from "next-auth/providers/email";
+
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import { prisma } from "@/src/lib/db"
+
+const handler = NextAuth({
+    adapter: PrismaAdapter(prisma),
+    providers: [
+        EmailProvider({
+            server: {
+                host: process.env.SMTP_HOST,
+                port: process.env.SMTP_PORT,
+                auth: {
+                    user: process.env.SMTP_USER,
+                    pass: process.env.SMTP_PASSWORD,
+                },
+            },
+            from: process.env.EMAIL_FROM,
+        }),
+        GithubProvider({
+            clientId: process.env.GITHUB_ID as string,
+            clientSecret: process.env.GITHUB_SECRET as string,
+        }),
+    ],
+});
+
+export { handler as GET, handler as POST };
--- src/pages/api/id/index.ts
+++
@@ -1,8 +0,0 @@
-import { getServerSession } from "next-auth"
-import { NextApiRequest, NextApiResponse } from "next"
-import { authOptions } from "../auth/[...nextauth]"
-
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const session = await getServerSession(authOptions)
-  return res.json(session)
-}
