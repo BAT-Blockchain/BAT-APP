d2e54b5e9bc770b45aee4b1e18e5e41d9e62a83c
juanifogo <juanifogo@gmail.com>
2023-11-14 13:30:40 -0300

    add email provider

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -4,22 +4,23 @@ import GoogleProvider from "next-auth/providers/google";
 import EmailProvider from "next-auth/providers/email";
 
 import { PrismaAdapter } from "@auth/prisma-adapter"
-import { prisma } from "@/src/lib/db"
+import { PrismaClient } from "@prisma/client";
 
+const prisma = new PrismaClient()
 const handler = NextAuth({
-    // adapter: PrismaAdapter(prisma),
+    adapter: PrismaAdapter(prisma),
     providers: [
-        // EmailProvider({
-        //     server: {
-        //         host: process.env.SMTP_HOST,
-        //         port: process.env.SMTP_PORT,
-        //         auth: {
-        //             user: process.env.SMTP_USER,
-        //             pass: process.env.SMTP_PASSWORD,
-        //         },
-        //     },
-        //     from: process.env.EMAIL_FROM,
-        // }),
+        EmailProvider({
+            server: {
+                host: process.env.SMTP_HOST,
+                port: process.env.SMTP_PORT,
+                auth: {
+                    user: process.env.SMTP_USER,
+                    pass: process.env.SMTP_PASSWORD,
+                },
+            },
+            from: process.env.EMAIL_FROM,
+        }),
         GithubProvider({
             clientId: process.env.GITHUB_ID as string,
             clientSecret: process.env.GITHUB_SECRET as string,
